36fa04ec025dadddb057b3fe363e2e9d31d1b85c
Kara Bayse <karabayse@gmail.com>
2017-10-25 14:20:14 -0500

    dot notation versus brackets

--- client.js
+++ client.js
@@ -1,1 +1,69 @@
 console.log('client.js');
+
+// object:  a data structure in which to store data
+// can store properties
+// key value pairs --> key is a string
+
+
+
+// PROPERTY ACCESS
+// BRACKET NOTATION
+// DOT NOTATION
+// DOT VS BRACKET
+// NESTED OBJECTS
+// OBJECT LITERALS
+// ITERATION  -->  aka LOOPING
+
+
+
+// empty object stored in a variable
+// object literal notation
+var box = {};
+
+// DOT NOTATION
+// use dots to access methods
+// use dots to access properties
+// whatever is to the left of a dot, is an object
+box.material = 'cardboard';
+
+// box.material returns 'cardboard' --> checks memory and returns what is stored
+console.log(box.material);
+
+var cb = box.material;
+// cb returns 'cardboard'
+console.log(cb);
+
+// change box.material to 'titanium'
+box.material = 'titanium';
+// still returns 'cardboard', because it is stored by value
+// checks memory and returns what is stored
+console.log(cb);
+
+// box.size;
+// returns undefined (undefined is its own type)
+
+
+
+// BRACKETS
+// 2 examples below are interchangeable
+box['material'] = 'cardboard';
+
+box.material;
+
+var key = 'material';
+// logs 'cardboard'
+console.log(box[key]);
+// logs 'undefined'  -->  looks up 'key' as a property on the object 'box'
+console.log(box['key']);
+// logs 'undefined'  -->  looks up 'key' as a property on the object 'box'
+console.log(box.key);
+// logs 'cardboard'
+console.log(box[key]);
+
+
+// EXPRESSIONS
+var func = function() {
+  return 'material';
+};
+// logs 'cardboard'
+console.log(box[func()]);
